47319d012d39f29d00868f6cf89ace52fdd222c3
AlexA <donburbon@bk.ru>
2020-05-25 23:54:55 +0500

    comp slider

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel__iner').slick({
+            speed: 1200,
+            adaptiveHeight: true,
+            prevArrow:'<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+            nextArrow:'<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+            responsive: [{
+                breakpoint: 992,
+                settings: {
+                    dots:true,
+                    arrows: false
+                }
+            } 
+        ]     
+        });
+  });
